cd496920eaefca6b5f41b814abddd01eb85e0bb4
Nastena <anastasia.stepanovich@outlook.com>
2026-02-11 15:42:26 +0300

    Added User class

--- server/src/main/java/com/nastena/pawsitive/server/ServerApplication.java
+++ server/src/main/java/com/nastena/pawsitive/server/ServerApplication.java
@@ -1,13 +1,13 @@
 package com.nastena.pawsitive.server;
 
+import com.nastena.pawsitive.server.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
 }
---
+++ server/src/main/java/com/nastena/pawsitive/server/model/User.java
@@ -0,0 +1,69 @@
+package com.nastena.pawsitive.server.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        OWNER, SHELTER
+    }
+
+    public User() {}
+    public User(String username, String email, String passwordHash, Role role) {
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    
+
+
+
+}
