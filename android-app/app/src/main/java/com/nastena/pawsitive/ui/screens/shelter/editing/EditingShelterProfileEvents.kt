68da7f7aeba95c719642359ba429dbfd9fda5f00
Nastena <anastasia.stepanovich@outlook.com>
2026-03-22 17:34:27 +0300

    Added editing package

---
+++ android-app/app/src/main/java/com/nastena/pawsitive/ui/screens/shelter/editing/EditingShelterProfileEvents.kt
@@ -0,0 +1,8 @@
+package com.nastena.pawsitive.ui.screens.shelter.editing
+
+import com.nastena.pawsitive.ui.screens.register.RegisterViewEvents
+import com.nastena.pawsitive.ui.screens.user.profile.UserProfileViewEvents
+
+sealed interface EditingShelterProfileEvents {
+
+}
